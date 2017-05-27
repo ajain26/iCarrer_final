bceeb77f18bdc5e7d085c1e96fcf86690fe389a0
ajain26 <rpsindia@rpsindias-iMac-2.local>
2017-05-27 23:11:47 +0530

    creating new project

---
+++ ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  iCarrer
+//
+//  Created by rpsindia on 27/05/17.
+//  Copyright © 2017 iCareer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        NSLog(@"anshul");
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
